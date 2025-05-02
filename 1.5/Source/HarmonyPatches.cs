0be7b1055cacc9cfba80cdc7756b1ccef9823227
Taranchuk <nfhfyxer@gmail.com>
2025-05-02 14:29:09 +0300

    regular shambler raid fix

--- 1.5/Source/HarmonyPatches.cs
+++ 1.5/Source/HarmonyPatches.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 using RimWorld;
+using StaggeredRaids;
 using UnityEngine;
 using Verse;
 
@@ -38,7 +39,11 @@ namespace StaggeredRaids
 
                 PawnGroupMaker groupMaker = parms.faction.def.pawnGroupMakers.FirstOrDefault(
                     x => x.kindDef == PawnGroupKindDefOf.Combat);
-
+                if (__instance is IncidentWorker_ShamblerAssault)
+                {
+                    groupMaker = parms.faction.def.pawnGroupMakers.FirstOrDefault(
+                    x => x.kindDef == PawnGroupKindDefOf.Shamblers);
+                }
                 if (groupMaker == null)
                     return true;
 
@@ -60,6 +65,56 @@ namespace StaggeredRaids
             }
 
             return true;
+        }
+    }
+
+
+
+    [HarmonyPatch(typeof(IncidentWorker_EntitySwarm), "TryExecuteWorker")]
+    public static class IncidentWorker_EntitySwarm_TryExecuteWorker_Patch
+    {
+        public static void Prefix(IncidentWorker_EntitySwarm __instance, ref IncidentParms parms)
+        {
+            if (!ModsConfig.AnomalyActive || !(parms.target is Map map) ||
+                __instance is IncidentWorker_ShamblerSwarmSmall)
+                return;
+            PawnGroupKindDef groupKindDef = __instance.GroupKindDef;
+            FloatRange swarmSizeVariance = __instance.SwarmSizeVariance;
+            float adjustedPoints = parms.points * swarmSizeVariance.RandomInRange;
+            adjustedPoints = Mathf.Max(adjustedPoints,
+                Faction.OfEntities.def.MinPointsToGeneratePawnGroup(groupKindDef) * 1.05f);
+            PawnGroupMakerParms pawnGroupMakerParms = new PawnGroupMakerParms
+            {
+                groupKind = groupKindDef,
+                tile = map.Tile,
+                faction = Faction.OfEntities,
+                points = adjustedPoints
+            };
+            int estimatedEntities = PawnGroupMakerUtility.GeneratePawnKindsExample(pawnGroupMakerParms).Count();
+            if (estimatedEntities > StaggeredRaidsUtility.MaxRaidersPerWave)
+            {
+                int numWaves = Mathf.CeilToInt((float)estimatedEntities / StaggeredRaidsUtility.MaxRaidersPerWave);
+                float pointsPerWave = adjustedPoints / numWaves;
+                parms.points = pointsPerWave;
+                for (int i = 1; i < numWaves; i++)
+                {
+                    int delay = i * StaggeredRaidsUtility.TicksBetweenWaves;
+                    IncidentParms waveParms = new IncidentParms
+                    {
+                        target = map,
+                        points = pointsPerWave,
+                        faction = parms.faction,
+                        forced = true,
+                        questTag = parms.questTag
+                    };
+                    if (!StaggeredRaidsUtility.pendingRaidWaves.ContainsKey(map))
+                    {
+                        StaggeredRaidsUtility.pendingRaidWaves[map] = new RaidGroup();
+                    }
+
+                    StaggeredRaidsUtility.pendingRaidWaves[map].waves.Add(new RaidWaveInfo(__instance.def, waveParms, delay));
+                }
+            }
         }
     }
 
